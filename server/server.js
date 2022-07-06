281bbc704f3bd07bcc4e03de557b1cd7d6d9af75
Brain <ortiz_brian323@yahoo.com>
2022-07-05 20:31:33 -0700

    added Requirements for Project 3

---
+++ server/server.js
@@ -0,0 +1,29 @@
+
+
+/*
+Your project must meet the following requirements:
+
+Use React for the front end.
+
+Use GraphQL with a Node.js and Express.js server.
+
+Use MongoDB and the Mongoose ODM for the database.
+
+Use queries and mutations for retrieving, adding, updating, and deleting data.
+
+Be deployed using Heroku (with data).
+
+Have a polished UI.
+
+Be responsive.
+
+Be interactive (i.e., accept and respond to user input).
+
+Include authentication (using JWT).
+
+Protect sensitive API key information on the server.
+
+Have a clean repository that meets quality coding standards (file structure, naming conventions, best practices for class and id naming conventions, indentation, high-quality comments, etc.).
+
+Have a high-quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
+*/
