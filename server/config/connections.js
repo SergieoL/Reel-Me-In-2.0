eb77b719db227e616ed4ea84f9f1c69c8ea17fcc
Brain <ortiz_brian323@yahoo.com>
2022-07-05 20:36:04 -0700

    added connection file

---
+++ server/config/connections.js
@@ -0,0 +1,13 @@
+//requires mongoose
+const mongoose = require('mongoose');
+
+//connecting to MongoDB
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/Reel-Me-In-2.0',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+//exporting to server.js
+module.exports = mongoose.connection;
