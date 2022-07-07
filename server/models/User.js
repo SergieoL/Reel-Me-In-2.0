f30859b19caed51eebf8a38519d4565d544055c5
Sergieo <sergieol23@gmail.com>
2022-07-07 00:55:17 -0500

    created user model

---
+++ server/models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address.']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8
+        },
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Review'
+            }
+        ],
+        savedMovies: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Movie'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+// middleware to create password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare new password with hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
