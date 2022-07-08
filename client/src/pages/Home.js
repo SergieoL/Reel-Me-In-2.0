b3a9c60cb963933487ab2310f23d335913583f90
Brain <ortiz_brian323@yahoo.com>
2022-07-08 00:25:34 -0700

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 //import statements
 import React from 'react';
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import WatchList from '../components/WatchList';
 
 
 const Home = () => {
     return(
-        //JSX goes here
+        //JSX goes for homepage goes here
+        //use Auth to check if user is logged in and adjust JSX accordingly 
         <></>
     )
 };
