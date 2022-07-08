57cc1770edb512ad15b9926c1127efbc0b106c0d
Brain <ortiz_brian323@yahoo.com>
2022-07-07 22:43:36 -0700

    Checking if user is still logged in

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,8 +5,15 @@ class AuthService {
     getProfile() {
         return decode(this.getToken());
     }
+    // check if the user is still logged in
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken();
+        // use type coersion to check if token is NOT undefined and the token is NOT expired
+        return !!token && !this.isTokenExpired(token);
+    }
 
-
+    
 }
 
 export default new AuthService();
