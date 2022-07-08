3c61fa8333b32e8a6c34cc01f0a4178a731425b0
Brain <ortiz_brian323@yahoo.com>
2022-07-08 00:46:01 -0700

    added homepage skeleton

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,23 @@
 import React from 'react';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
+import { QUERY_WATCHLIST, QUERY_ME_BASIC } from '../utils/queries';
 import WatchList from '../components/WatchList';
+import ReviewForm from '../components/ReviewForm';
+import ReviewList from '../components/ReviewList';
+import CommentForm from '../components/CommentForm';
+import CommentList from '../components/CommentList'
 
 
 const Home = () => {
-    return(
+    //use these to query and update homepage in JSX
+ const { loading, data } = useQuery(QUERY_WATCHLIST);
+ const { data: userData } = useQuery(QUERY_ME_BASIC);
+ const loggedIn = Auth.loggedIn();
+
+    return (
         //JSX goes for homepage goes here
+        //use eventhandlers to change between comment/review forms 
         //use Auth to check if user is logged in and adjust JSX accordingly 
         <></>
     )
