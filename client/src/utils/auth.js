e0a45403345a03009fb47a6e83b3c20a8e2f87c8
Brain <ortiz_brian323@yahoo.com>
2022-07-07 22:44:39 -0700

    completed auth file for client side.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -13,7 +13,42 @@ class AuthService {
         return !!token && !this.isTokenExpired(token);
     }
 
-    
+    // check if the token has expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    // retrieve token from localStorage
+    getToken() {
+        // Retrieves the user token from localStorage
+        return localStorage.getItem('id_token');
+    }
+
+    // set token to localStorage and reload page to homepage
+    login(idToken) {
+        // Saves user token to localStorage
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // clear token from localStorage and force logout with reload
+    logout() {
+        // Clear user token and profile data from localStorage
+        localStorage.removeItem('id_token');
+        // this will reload the page and reset the state of the application
+        window.location.assign('/');
+    }
+
 }
 
 export default new AuthService();
