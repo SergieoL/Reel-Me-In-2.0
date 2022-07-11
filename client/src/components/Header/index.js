e9f86d9300b13148544b2470d6bbb50810363560
Sergieo <sergieol23@gmail.com>
2022-07-10 22:59:44 -0500

    started header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="">
+      <div className="">
+        <h1>Reel Me In 2.0</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
