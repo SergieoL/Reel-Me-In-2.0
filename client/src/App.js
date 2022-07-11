4f1ceadcfd8361b25714dedfa819cc37adce7271
Sergieo <sergieol23@gmail.com>
2022-07-10 23:07:48 -0500

    fixed homepage rendering issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-
+        <Home />
       </div>
     </div>
   )
