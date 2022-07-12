a9c590fafead19dff13c67ce722a628d4a86ea28
Akimpson <akimp313@gmail.com>
2022-07-12 19:05:06 -0400

    Create login.js sign-in

---
+++ client/reel-me-in-2.0/js/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import loginpic from " ../images/login.svg";
+
+const Login = () => {
+    return (
+        <>
+            <section className="sign-in">
+                <div className="container mt-5"></div>
+                <div className="signin-content"></div>
+
+                
+            </section>
+    )
+}
