de0f8da0180afb228f4fc281663f7e58d7caedba
Akimpson <akimp313@gmail.com>
2022-07-12 19:28:47 -0400

    added form-group Your Name

--- client/reel-me-in-2.0/js/login.js
+++ client/reel-me-in-2.0/js/login.js
@@ -5,21 +5,30 @@ const Login = () => {
     return (
         <>
             <section className="sign-in">
-                <div className="container mt-5"></div>
-                <div className="signin-content"></div>
+                <div className="container mt-5">
+                <div className="signin-content">
 
                     <div className="signin-image">
                         <figure>
                             <img src={loginpic} alt="Login pic" />
                         </figure>
-                        <NavLink to="/signup" className="signup-image-link">Create an A</NavLink>
+                        <NavLink to="/signup" className="signup-image-link">Create an Account</NavLink>
                     </div>
 
                     <div className="signin-form">
                         <h2 className="form-title">Sign up</h2>
                         <form className="register-form" id="register-form">
 
-                            
+                            <div className="form-group">
+                                <label htmlFor="name"></label>
+                                    <i className="zmdi zmdi-account material-icons-name"></i>
+                                </label>
+                                <input type="text" name="name" id="name" autoComplete="off"
+                                    placeholder="Your Name"
+                                />
+                            </div>    
+
+
                     </div>
             </section>
     )
