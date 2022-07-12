31a2edb22dba7f37a4ae56390b4bab69821c1a0b
Akimpson <akimp313@gmail.com>
2022-07-12 15:42:37 -0400

    added signup.js

--- client/reel-me-in-2.0/js/signup.js
+++ client/reel-me-in-2.0/js/signup.js
@@ -1,5 +1,12 @@
+import React from 'react'
 
-
-
-
+const Signup = () => {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1>We Are The <span className="justfordemo">Registration </span>  Developer</h1>
+        </div>
+    )
+}
+export default Signup
 }
