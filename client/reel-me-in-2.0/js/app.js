38401139f085fb6288c2e2f097640e2caed844f5
Akimpson <akimp313@gmail.com>
2022-07-12 13:45:31 -0400

    added App function in app.js

--- client/reel-me-in-2.0/js/app.js
+++ client/reel-me-in-2.0/js/app.js
@@ -7,3 +7,28 @@ import About from ".components/About";
 import Contact from ".components/Contact";
 import Login from ".components/Login";
 import Signup from ".components/Signup";
+
+
+
+const App = () => {
+    return (
+        <>
+        <Navbar  />
+
+        <Route exact path="/">
+            <Home  />
+        </Route>
+
+        <Route path="/about">
+            <About  />
+        </Route>
+
+        <Route path="/contact">
+            <Contact  />
+        </Route>
+
+        <Route path="/login">
+            <Login  />
+        </Route>
+    )
+}
