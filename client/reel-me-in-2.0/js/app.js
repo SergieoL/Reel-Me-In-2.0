6544aafb74a896fc73aa185c7c0dc310250ab892
Akimpson <akimp313@gmail.com>
2022-07-12 13:43:31 -0400

    added App.js

---
+++ client/reel-me-in-2.0/js/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import ".App.css";
+import Navbar from ".components/Navbar";
+import Home from ".components/Home";
+import About from ".components/About";
+import Contact from ".components/Contact";
+import Login from ".components/Login";
+import Signup from ".components/Signup";
