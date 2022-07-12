c8fd6c8c89dfe5b5a23a75107f88685a293f9381
Akimpson <akimp313@gmail.com>
2022-07-12 19:20:34 -0400

    updated login.js signin-form

--- client/reel-me-in-2.0/js/login.js
+++ client/reel-me-in-2.0/js/login.js
@@ -8,7 +8,19 @@ const Login = () => {
                 <div className="container mt-5"></div>
                 <div className="signin-content"></div>
 
-                
+                    <div className="signin-image">
+                        <figure>
+                            <img src={loginpic} alt="Login pic" />
+                        </figure>
+                        <NavLink to="/signup" className="signup-image-link">Create an A</NavLink>
+                    </div>
+
+                    <div className="signin-form">
+                        <h2 className="form-title">Sign up</h2>
+                        <form className="register-form" id="register-form">
+
+                            
+                    </div>
             </section>
     )
 }
