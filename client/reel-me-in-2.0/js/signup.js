aef7c6817c0ae03ea58f429de6c8e1556ad1978e
Akimpson <akimp313@gmail.com>
2022-07-12 17:00:47 -0400

    added register button in js

--- client/reel-me-in-2.0/js/signup.js
+++ client/reel-me-in-2.0/js/signup.js
@@ -10,64 +10,93 @@ const Signup = () => {
             <div className="signup-form">
               <h2 className="form-title">Sign up</h2>
               <form className="register-form" id="register-form">
-
                 <div className="form-group">
                   <label htmlFor="name">
-                    < i className="zmdi zmdi-account material-icons-name"></i>
+                    <i className="zmdi zmdi-account material-icons-name"></i>
                   </label>
-                  <input type="text" name="name" id="name" autoComplete="off"
-                  placeholder="Your Name"
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="off"
+                    placeholder="Your Name"
                   />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="email">
-                        <i className="zmdi zmdi-email material-icons-name"></i>
+                  <label htmlFor="email">
+                    <i className="zmdi zmdi-email material-icons-name"></i>
                   </label>
-                  <input type="email" name="email" id="email" autoComplete="off"
-                  placeholder="Your Email"
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    placeholder="Your Email"
                   />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="phone">
-                        <i className="zmdi zmdi-phone material-icons-name"></i>
+                  <label htmlFor="phone">
+                    <i className="zmdi zmdi-phone material-icons-name"></i>
                   </label>
-                  <input type="phone" name="phone-in -talk" id="phone" autoComplete="off"
-                  placeholder="Your Phone"
+                  <input
+                    type="phone"
+                    name="phone-in -talk"
+                    id="phone"
+                    autoComplete="off"
+                    placeholder="Your Phone"
                   />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="work">
-                        <i className="zmdi zmdi-slideshow material-icons-name"></i>
+                  <label htmlFor="work">
+                    <i className="zmdi zmdi-slideshow material-icons-name"></i>
                   </label>
-                  <input type="text" name="work" id="work" autoComplete="off"
-                  placeholder="Your Profession"
+                  <input
+                    type="text"
+                    name="work"
+                    id="work"
+                    autoComplete="off"
+                    placeholder="Your Profession"
                   />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="password">
-                        <i className="zmdi zmdi-lock material-icons-name"></i>
+                  <label htmlFor="password">
+                    <i className="zmdi zmdi-lock material-icons-name"></i>
                   </label>
-                  <input type="password" name="password" id="password" autoComplete="off"
-                  placeholder="Your Password"
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    placeholder="Your Password"
                   />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="cpassword">
-                        <i className="zmdi zmdi-lock material-icons-name"></i>
+                  <label htmlFor="cpassword">
+                    <i className="zmdi zmdi-lock material-icons-name"></i>
                   </label>
-                  <input type="password" name="cpassword" id="cpassword" autoComplete="off"
-                  placeholder="Confirm Your Password"
+                  <input
+                    type="password"
+                    name="cpassword"
+                    id="cpassword"
+                    autoComplete="off"
+                    placeholder="Confirm Your Password"
                   />
                 </div>
 
-
-
-                    </label>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signup"
+                    id="signup"
+                    className="form-submit"
+                    value="register"
+                  />
+                </div>
               </form>
             </div>
           </div>
