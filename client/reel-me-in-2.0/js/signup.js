e52ef3b635e2a1b847b60b83795d001993148059
Akimpson <akimp313@gmail.com>
2022-07-12 16:45:49 -0400

    added signup section in js

--- client/reel-me-in-2.0/js/signup.js
+++ client/reel-me-in-2.0/js/signup.js
@@ -10,9 +10,20 @@ const Signup = () => {
             <div className="signup-form">
               <h2 className="form-title">Sign up</h2>
               <form className="register-form" id="register-form">
+
                 <div className="form-group">
-                  <label htmlFor="name"></label>
+                  <label htmlFor="name">
+                    < i className="zmdi zmdi-account material-icons-name"></i>
+                  </label>
+                  <input type="text" name="name" id="name" autoComplete="off"
+                  placeholder="Your Name"
+                  />
                 </div>
+
+        
+
+
+                    </label>
               </form>
             </div>
           </div>
