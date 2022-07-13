416ba233b785f69678ee468ab2514a7df5cd221f
mikak <mekawaters@yahoo.com>
2022-07-13 01:10:22 -0400

    more css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../../../src/index.css";
 
 const Header = () => {
   return (
-    <header className="">
-      <div className="">
+    <header className="header">
+      <div className="header-text">
         <Link to='/'>
           <h1>Reel Me In 2.0</h1>
         </Link>
 
-        <nav className=''>
+        <nav className='link-text'>
           <Link to='/login'>Login</Link>
           
           <Link to='/signup'>Signup</Link>
