f2169554e3b1a8f9e35fa59bca17a2abc71bd2a6
mikak <mekawaters@yahoo.com>
2022-07-13 17:35:41 -0400

    added css to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
   }
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-primary text-decoration-none text-danger text-center mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to='/'>
           <h1>Reel Me In 2.0</h1>
@@ -22,17 +22,17 @@ const Header = () => {
         <nav className='text-right'>
           {Auth.loggedIn() ? (
             <>
-              <Link to="/profile">My Profile</Link>
-              <Link to='/newreview'>Create Review</Link>
-              <a href='/' onClick={logout}>
+              <Link to="/profile" className='px-2'>My Profile</Link>
+              <Link to='/newreview' className='px-2'>Create Review</Link>
+              <a href='/' className='px-2' onClick={logout}>
                 Logout
               </a>
             </>
           ) : (
             <>
-            <Link to='/login'>Login</Link>
+            <Link to='/login' className='px-2'>Login</Link>
             
-            <Link to='/signup'>Signup</Link>
+            <Link to='/signup' className='px-2'>Signup</Link>
             </>
           )}
         </nav>
