10909c6f6e77dd0b5a04eac3688e165738528603
Brain <ortiz_brian323@yahoo.com>
2022-07-12 20:16:54 -0700

    modified login page, add JSX, Login works

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,38 @@
+//imports
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Login = () => {
+  //declarations for state and login authentication
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+  //handling changes by user
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  // handling on submit
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+  };
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -11,20 +40,15 @@ const Login = () => {
         <div className='card'>
           <h4 className='card-header'>Login</h4>
           <div className='card-body'>
-            <form>
-              <input
-                className='form-input'
-                placeholder='Your username'
-                name='username'
-                type='username'
-                id='username'
-              />
+            <form onSubmit={handleFormSubmit}>
               <input
                 className='form-input'
                 placeholder='Your email'
                 name='email'
                 type='email'
                 id='email'
+                value={formState.email}
+                onChange={handleChange}
               />
               <input
                 className='form-input'
@@ -32,10 +56,13 @@ const Login = () => {
                 name='password'
                 type='password'
                 id='password'
+                value={formState.password}
+                onChange={handleChange}
               />
               <button className='btn d-block w-100' type='submit'>
                 Submit
               </button>
+              {error && <div>Login failed</div>}
             </form>
           </div>
         </div>
