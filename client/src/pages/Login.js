44718c32d107e9929a01cd8dd6641d365fdfa2fe
Brain <ortiz_brian323@yahoo.com>
2022-07-12 19:27:57 -0700

    added mutations for signup/login

---
+++ client/src/pages/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+
+const Login = () => {
+
+  return (
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-md-6'>
+        <div className='card'>
+          <h4 className='card-header'>Login</h4>
+          <div className='card-body'>
+            <form>
+              <input
+                className='form-input'
+                placeholder='Your username'
+                name='username'
+                type='username'
+                id='username'
+              />
+              <input
+                className='form-input'
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+              />
+              <button className='btn d-block w-100' type='submit'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
