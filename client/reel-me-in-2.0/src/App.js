b3f424773d6dc569e3500bdba3206b37698ebb2a
Markeicegreen16 <98359896+Markeicegreen16@users.noreply.github.com>
2022-07-14 18:18:14 -0400

    Update App.js

--- client/reel-me-in-2.0/src/App.js
+++ client/reel-me-in-2.0/src/App.js
@@ -10,6 +10,7 @@ import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
+    
     <GlobalProvider>
       <Router>
         <Header />
@@ -28,6 +29,22 @@ function App() {
       </Router>
     </GlobalProvider>
   );
+  <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
 }
 
 export default App;
